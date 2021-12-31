2cf505fce6173b32a5b14f2df08e7832ce4e3f76
Nutifafa Attor <attorfafa@gmail.com>
2021-12-31 15:32:47 +0000

    update

--- js/Easy/food.js
+++ js/Easy/food.js
@@ -1,15 +1,35 @@
-let food = { x:0, y:0}
+import {onSnake, expandSnake} from './snake.js'
+
+let food = { x: 10, y: 1 }
+const EXPANSION_RATE = 1    // how much the snake grows when it eats the food
+const score = document.getElementById('score')
+let scoreCount = 0
+score.innerHTML = scoreCount
+
+
 
 export function update() {
-   
+    if (onSnake(food)) {
+        expandSnake(EXPANSION_RATE)
+        scoreCount = scoreCount + 2
+        food = { x: 20, y: 10 }
+        
+        
+    }
 }
 
+score.innerHTML = scoreCount
+
 export function draw(gameBoard) {
-    
+
     const foodElement = document.createElement('div')
     foodElement.style.gridRowStart = food.y
     foodElement.style.gridColumnStart = food.x
     foodElement.classList.add('food')
     gameBoard.appendChild(foodElement)
+
+}
+
+function getRandomFoodPosition() {
     
 }
--- js/Easy/snake.js
+++ js/Easy/snake.js
@@ -1,19 +1,20 @@
 import { getInputDirection } from "./input.js"
 
-export const SNAKE_SPEED = 1   // How many times the snake will move per second
+export const SNAKE_SPEED = 3   // How many times the snake will move per second
 const snakeBody = [
     { x: 11, y: 11 }
-    
 ]
+let newSegments = 0
 
 export function update() {
+    addSegments()
     const inputDirection = getInputDirection()
     for (let i = snakeBody.length - 2; i >= 0; i--) {
-        snakeBody[i + 1] = {...snakeBody[i]}    // Create a duplicate of snake at position i and set it to snake at position i + 1 (goes up by one)
+        snakeBody[i + 1] = { ...snakeBody[i] }    // Create a duplicate of snake at position i and set it to snake at position i + 1 (goes up by one)
     }
 
     // 0 is the first segment of snake in array
-    snakeBody[0].x += inputDirection.x 
+    snakeBody[0].x += inputDirection.x
     snakeBody[0].y += inputDirection.y
 }
 
@@ -25,4 +26,26 @@ export function draw(gameBoard) {
         snakeElement.classList.add('snake')
         gameBoard.appendChild(snakeElement)
     })
+}
+
+export function expandSnake(amount) {
+    newSegments += amount
+}
+
+export function onSnake(position){
+    return snakeBody.some((segment => {
+        return equalPositions(segment, position)
+    }))
+}
+
+function equalPositions(pos1, pos2){
+    return pos1.x === pos2.x && pos1.y === pos1.y  
+}
+
+function addSegments() {
+    for (let i = 0; i < newSegments; i++) {
+        snakeBody.push({ ...snakeBody[snakeBody.length - 1] })
+    }
+
+    newSegments = 0     //to get rid of continuous addition of new segment 
 }
